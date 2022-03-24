1d4272a2a44768cf9f4e327ada644d9d1a94176c
Anjan Kailash <anjan.kailash@gmail.com>
2022-03-24 08:55:48 +0530

    Revert "Revert "Revert "all files got deleted"""
    This reverts commit 08fb351dc235695c338de6c841da8e13e372a940.

---
+++ spring-boot-testing/src/main/java/net/javaguides/springboottesting/repository/EmployeeRepository.java
@@ -0,0 +1,28 @@
+package net.javaguides.springboottesting.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import net.javaguides.springboottesting.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+	Optional<Employee> findByEmail(String email);
+	
+	//jpql custom query with index parameters
+	@Query("select e from Employee e where e.firstName = ?1 and e.lastName = ?2")
+	Employee findByJPQL(String firstName, String lastName);
+	
+	//jpql custom query with named parameters
+	@Query("select e from Employee e where e.firstName =:firstName and e.lastName =:lastName")
+	Employee findByJPQLNamedParams(@Param("firstName")String firstName, @Param("lastName")String lastName);
+
+	@Query(value = "select * from employees e where e.first_name =?1 and e.last_name =?2", nativeQuery=true)
+	Employee findByNativeQuery(String firstName, String lastName);
+	
+	@Query(value = "select * from employees e where e.first_name =:firstName and e.last_name =:lastName", nativeQuery=true)
+	Employee findByNativeQueryNamedParams(@Param("firstName")String firstName, @Param("lastName")String lastName);
+}
